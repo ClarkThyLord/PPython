aa49ed40b964af64a07f8308f3815b5106008dcc
Christian Cuevas <ccpotential@gmail.com>
2021-09-28 16:35:33 -0700

    Add bootstrap and draft UI

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,80 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-100 d-flex flex-column">
+    <nav class="navbar navbar-light bg-light">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="">
+          <img
+            src="../public/ppython.svg"
+            alt=""
+            width="30"
+            height="24"
+            class="d-inline-block align-text-top"
+          />
+          PPython
+        </a>
+      </div>
+    </nav>
+
+    <div class="flex-fill p-3">
+      <div class="row h-100">
+        <div class="col d-flex flex-column">
+          <div class="hstack gap-3 mx-3">
+            <img
+              src="../public/ppython.svg"
+              alt=""
+              height="48"
+              class="d-inline-block align-text-top"
+            />
+            <h1>PPython</h1>
+            <a href="#help" class="link-info">?</a>
+            <div class="ms-auto btn-group">
+              <button type="button" class="btn btn-danger">Clear</button>
+              <button type="button" class="btn btn-success">Transpile</button>
+            </div>
+          </div>
+          <textarea class="form-control flex-fill" id="pythonSource"></textarea>
+        </div>
+        <div class="col d-flex flex-column">
+          <div class="hstack gap-3">
+            <h1>C++</h1>
+            <div class="ms-auto btn-group me-2">
+              <button type="button" class="btn btn-primary active">Code</button
+              ><button type="button" class="btn btn-danger position-relative">
+                Errors
+                <span
+                  class="
+                    position-absolute
+                    top-0
+                    start-100
+                    translate-middle
+                    badge
+                    rounded-pill
+                    bg-warning
+                  "
+                >
+                  99+
+                </span>
+              </button>
+            </div>
+          </div>
+          <textarea class="form-control flex-fill" id="pythonSource"></textarea>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+};
 </script>
 
 <style>
+html,
+body,
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  widows: 100%;
+  height: 100%;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+import 'bootstrap/dist/css/bootstrap.min.css'
+import * as boostrap from 'bootstrap'
+window.bootstrap = boostrap
+
 createApp(App).mount('#app')
