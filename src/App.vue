adae2cf9b8dda44d85ac6d05ad1551d2da71fa88
Christian Cuevas <ccpotential@gmail.com>
2021-09-28 21:14:59 -0700

    Refine UI

--- src/App.vue
+++ src/App.vue
@@ -1,72 +1,68 @@
 <template>
-  <div class="h-100 d-flex flex-column">
-    <nav class="navbar navbar-light bg-light">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="">
+  <div class="h-100 d-grid gap-3 p-3">
+    <div class="row h-100">
+      <div class="col-lg-6 d-flex flex-column">
+        <div class="hstack gap-3 mx-3">
           <img
             src="../public/ppython.svg"
             alt=""
-            width="30"
-            height="24"
-            class="d-inline-block align-text-top"
+            height="48"
+            class="d-inline-block align-text-top ppython-icon"
           />
-          PPython
-        </a>
-      </div>
-    </nav>
+          <h1>PPython</h1>
 
-    <div class="flex-fill p-3">
-      <div class="row h-100">
-        <div class="col d-flex flex-column">
-          <div class="hstack gap-3 mx-3">
-            <img
-              src="../public/ppython.svg"
-              alt=""
-              height="48"
-              class="d-inline-block align-text-top"
-            />
-            <h1>PPython</h1>
-            <a href="#help" class="link-info">?</a>
-            <div class="ms-auto btn-group">
-              <button type="button" class="btn btn-danger">Clear</button>
-              <button type="button" class="btn btn-success">Transpile</button>
-            </div>
+          <a href="#help" class="link-info">?</a>
+          <div class="ms-auto btn-group">
+            <button type="button" class="btn btn-danger">Clear</button>
+            <button type="button" class="btn btn-success">Transpile</button>
           </div>
-          <textarea class="form-control flex-fill" id="pythonSource"></textarea>
         </div>
-        <div class="col d-flex flex-column">
-          <div class="hstack gap-3">
-            <h1>C++</h1>
-            <div class="ms-auto btn-group me-2">
-              <button type="button" class="btn btn-primary active">Code</button
-              ><button type="button" class="btn btn-danger position-relative">
-                Errors
-                <span
-                  class="
-                    position-absolute
-                    top-0
-                    start-100
-                    translate-middle
-                    badge
-                    rounded-pill
-                    bg-warning
-                  "
-                >
-                  99+
-                </span>
-              </button>
-            </div>
+        <hr />
+        <TextArea ref="ppythonSource" />
+      </div>
+
+      <div class="col-lg-6 d-flex flex-column">
+        <div class="hstack gap-3 mx-3">
+          <h1>C++</h1>
+
+          <div class="ms-auto btn-group">
+            <button type="button" class="btn btn-primary active">Code</button
+            ><button type="button" class="btn btn-danger position-relative">
+              Errors
+              <span
+                class="
+                  position-absolute
+                  top-0
+                  start-100
+                  translate-middle
+                  badge
+                  rounded-pill
+                  bg-warning
+                "
+              >
+                99+
+              </span>
+            </button>
           </div>
-          <textarea class="form-control flex-fill" id="pythonSource"></textarea>
         </div>
+        <hr />
+        <TextArea :disabled="true" ref="cppSource" />
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import TextArea from "./components/text_area.vue";
+
 export default {
   name: "App",
+  components: {
+    TextArea,
+  },
+  mounted() {
+    this.$refs.ppythonSource.$el.focus();
+  },
 };
 </script>
 
@@ -76,5 +72,21 @@ body,
 #app {
   widows: 100%;
   height: 100%;
+}
+
+@media (max-width: 400px) {
+  img.ppython-icon {
+    display: none !important;
+  }
+
+  .col-lg-6 {
+    padding-top: 1rem * 0.25;
+  }
+}
+
+@media (max-width: 768px) {
+  .ppython-icon {
+    height: 32px !important;
+  }
 }
 </style>
---
+++ src/components/text_area.vue
@@ -0,0 +1,28 @@
+<template>
+  <textarea
+    class="form-control flex-fill"
+    :disabled="disabled"
+    :placeholder="placeholder"
+    v-model="text"
+  ></textarea>
+</template>
+
+<script>
+export default {
+  name: "TextArea",
+  props: {
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+      default: "",
+    },
+    text: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
