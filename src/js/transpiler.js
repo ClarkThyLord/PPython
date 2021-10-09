ed4516b044bf2d6532cc1c4abd518e142f664396
D-Chenkov <ChenkovvoknehC@gmail.com>
2021-10-08 17:38:27 -0700

    Added Regex to file. Additional separation

--- src/js/transpiler.js
+++ src/js/transpiler.js
@@ -23,8 +23,82 @@ export default function transpiler(ppython_source) {
         .replaceAll("\n", " \n ")
         .replaceAll("\t", " \t ")
         .replaceAll(":", " : ")
+        .replaceAll("(", " ( ")
+        .replaceAll(")", " ) ")
+        .replaceAll("=", " = ")
+        .replaceAll("<=", " <= ")
+        .replaceAll("<", " < ")
+        .replaceAll(">", " > ")
+        .replaceAll(">=", " >= ")
+        .replaceAll("==", " == ")
+        .replaceAll("!=", " != ")
+        .replaceAll("*", " * ")
+        .replaceAll("+", " + ")
+        .replaceAll("-", " - ")
+        .replaceAll("/", " / ") 
         .split(/[ ]+/).filter(n => n);
 
+    //Logger to check what the word means
+    var TokenExpressions = {};
+
+
+    let RegInt = /(?<!.)(Int)(?!.)/;
+    let RegIntVal = /(?<!.)[0-9]+(?!.)/;
+    let RegStr = /(?<!.)(String)(?!.)/;
+    let RegStrVal = /(?<!.)(".*")(?!.)/;
+    let RegBul = /(?<!.)(Bool)(?!.)/;
+    let RegBulVal = /(?<!.)(True|False)(?!.)/;
+    let RegSum = /(?<!.)[+](?!.)/;
+    let RegSub = /(?<!.)[-](?!.)/;
+    let RegMlt = /(?<!.)[*](?!.)/;
+    let RegDiv = /(?<!.)[/](?!.)/;
+    let RegEql = /(?<!.)(==)(?!.)/;
+    let RegAsn = /(?<!.)(=)(?!.)/;
+    let RegDif = /(?<!.)(!=)(?!.)/;
+    let RegGrt = /(?<!.)[>](?!.)/;
+    let RegGte = /(?<!.)(>=)(?!.)/;
+    let RegLwt = /(?<!.)[<](?!.)/;
+    let RegLte = /(?<!.)(<=)(?!.)/;
+    let RegAnd = /(?<!.)(and)(?!.)/;
+    let RegOor = /(?<!.)(or)(?!.)/;
+    let RegWle = /(?<!.)(While)(?!.)/;
+    let RegIff = /(?<!.)(If)(?!.)/;
+    let RegEls = /(?<!.)(Else)(?!.)/;
+    let RegLif = /(?<!.)(Elif)(?!.)/;
+    let RegRtn = /(?<!.)([^\\d][a-zA-Z0-9]*)(?!.)/;
+    let RegBrk = /(?<!.)(Break)(?!.)/;
+    let RegVar = /(?<!.)([^d][a-zA-Z]*)(?!.)/;
+    let RegCom = /(\s(#[a-zA-Z0-9]*))/;
+    let RegDdt = /(?<!.)(:)(?!.)/;
+    let RegRgt =  /(?<!.)([(])(?!.)/;
+    let RegLft =  /(?<!.)([)])(?!.)/;  
+
+    
+    let state = 0;
+    let token_lex;
+   
+    for (let i = 0; i < raw_tokens.length; i++) { 
+        switch(state){
+            case 0: 
+
+            break;
+            case 1: 
+
+            break;
+            case 2:
+
+             
+
+            break;
+            case 3:
+
+             
+
+            break;
+        }
+        
+    }
+    
     cpp_source = JSON.stringify(raw_tokens, null, "\t");
 
     if (cpp_source === undefined) {
