61daca2092d0d1a572041006ec2061d15586aac6
Christian Cuevas <ccpotential@gmail.com>
2021-10-09 00:44:47 -0700

    Improve lexical analyzes

--- src/js/transpiler.js
+++ src/js/transpiler.js
@@ -36,7 +36,6 @@ export default function transpiler(ppython_source) {
         .replaceAll("/", " / ")
         .split(/[ ]+/).filter(n => n);
 
-    //Logger to check what the word means
     var TokenExpressions = {
         "identifier": {
             "variable_name": /(?<!.)[^d][a-zA-Z]*(?!.)/,
@@ -77,96 +76,29 @@ export default function transpiler(ppython_source) {
         },
     };
 
-    let tokens = [];
+    let lexical_tokens = [];
 
-    let state = 0;
-    let token_lex;
-
-    for (let i = 0; i < raw_tokens.length; i++) {
-        const raw_token = raw_tokens[i];
+    raw_tokens.forEach(raw_token => {
         console.log(raw_token);
+        let match = undefined;
+        Object.keys(TokenExpressions).some(token_name => {
+            console.log(token_name);
+            Object.keys(TokenExpressions[token_name]).some(token_expression => {
+                match = raw_token.match(token_expression);
+                console.log("Match: ", match);
+                if (match !== null && match.length == 1) {
+                    lexical_tokens.push([token_name, raw_token]);
+                    return true;
+                }
+            });
+            if (match)
+                return true;
+        });
+        if (!match)
+            logErrorMessage("Error: Syntax error");
+    });
 
-        for (let state = 0; state < Object.keys(TokenExpressions).length; state++) {
-            if (state == -1)
-                break;
-            console.log(state);
-            switch (state) {
-                case 0:
-                    for (const token_name in TokenExpressions["keyword"]) {
-                        const token_expression = TokenExpressions["keyword"][token_name];
-                        let match = raw_token.match(token_expression);
-                        console.log(match);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["keyword", raw_token])
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case 1:
-                    for (const token_name in TokenExpressions["operator"]) {
-                        const token_expression = TokenExpressions["operator"][token_name];
-                        let match = raw_token.match(token_expression);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["operator", raw_token]);
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case 2:
-                    for (const token_name in TokenExpressions["seperator"]) {
-                        const token_expression = TokenExpressions["seperator"][token_name];
-                        let match = raw_token.match(token_expression);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["seperator", raw_token])
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case 3:
-                    for (const token_name in TokenExpressions["literal"]) {
-                        const token_expression = TokenExpressions["literal"][token_name];
-                        let match = raw_token.match(token_expression);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["literal", raw_token]);
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case 4:
-                    for (const token_name in TokenExpressions["comment"]) {
-                        const token_expression = TokenExpressions["comment"][token_name];
-                        let match = raw_token.match(token_expression);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["comment", raw_token]);
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case 5:
-                    for (const token_name in TokenExpressions["identifier"]) {
-                        const token_expression = TokenExpressions["identifier"][token_name];
-                        let match = raw_token.match(token_expression);
-                        if (match !== null && match.length == 1) {
-                            tokens.push(["identifier", raw_token]);
-                            state = -1;
-                            break;
-                        }
-                    }
-                    break;
-                case -1:
-                    break;
-                default:
-                    logErrorMessage("Error: Syntax error `" + raw_token + "`")
-            }
-        }
-    }
-
-    cpp_source = JSON.stringify(tokens, null, "\t");
+    cpp_source = JSON.stringify(lexical_tokens, null, "\t");
 
     if (cpp_source === undefined) {
         logErrorMessage("Error: No C++ source code could be produced")
